da6d405bfb9d6a23cc43190d7d40638265bee909
Roaring1 <110640405+Roaring1@users.noreply.github.com>
2026-04-16 13:45:19 -0700

    Delete crucible directory

--- crucible/data/__init__.py
+++
@@ -1,5 +0,0 @@
-from .instance_model import ServerInstance
-from .instance_manager import InstanceManager
-from .backup_manager import BackupManager
-
-__all__ = ["ServerInstance", "InstanceManager", "BackupManager"]
